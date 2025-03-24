a1b6c3e0ea05e453b033f2937c5ac65eebfb496f
magfur2309 <magfurgezo@gmail.com>
2025-03-24 14:41:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -59,10 +59,11 @@ def extract_data_from_pdf(pdf_file, tanggal_faktur):
                             harga, qty, unit = 0.0, 0.0, "Unknown"
                         
                         total = harga * qty
-                        ppn = round(total * 0.11, 2)
-                        dpp = total - ppn
+                        dpp = round(total * 11 / 12, 2)
+                        ppn = round(dpp * 12 / 100, 2)
                         data.append([no_fp or "Tidak ditemukan", nama_penjual or "Tidak ditemukan", nama_pembeli or "Tidak ditemukan", tanggal_faktur, nama_barang, qty, unit, harga, total, dpp, ppn])
     return data
+
 
 def login_page():
     users = {
